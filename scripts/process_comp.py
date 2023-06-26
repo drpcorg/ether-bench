fc1ad5524b4d79dd217841177a644ac2c924dd34
a10zn8 <a1.cyb3r@gmail.com>
2023-06-26 17:48:04 +0400

    add errors to result and ignore temporary files

--- scripts/process_comp.py
+++ scripts/process_comp.py
@@ -5,13 +5,8 @@ import sys
 json_string = sys.stdin.read()
 data = json.loads(json_string)
 
-# Create the table header
-# table = "| Name | success | mean | p50 | p90 |\n| --- | --- | --- | --- | --- |\n"
-
-# Iterate over each stage and add a row to the table
-
 writer = csv.writer(sys.stdout)
-writer.writerow(['Name', 'success', 'mean', 'p50', 'p90'])
+writer.writerow(['Name', 'success', 'mean', 'p50', 'p90', 'errors'])
 
 for stage in data:
     name = stage['name']
@@ -19,5 +14,5 @@ for stage in data:
     mean = stage['stage_summary']['latencies']['mean'] / 1000000
     p50 = stage['stage_summary']['latencies']['50th'] / 1000000
     p90 = stage['stage_summary']['latencies']['90th'] / 1000000
-    writer.writerow([name, success, mean, p50, p90])
-
+    errors = ', '.join(stage['stage_summary']['errors'])
+    writer.writerow([name, success, mean, p50, p90, errors])
